91f2aac9adc998a2754609d35ba087aed8a88432
Sangroula <asang002@int.usc.local>
2016-11-22 10:24:03 -0600

    commit

---
+++ test_git/src/main/java/com/test/git/TestClass.java
@@ -0,0 +1,25 @@
+package com.test.git;
+
+public class TestClass {
+
+	public int add(int a, int b){
+		int result = a + b;
+		return result;
+	}
+	public int multiply(int a, int b){
+		int result = a*b;
+		return result;
+	}
+	public static void main(String[] args) {
+		TestClass ref = new TestClass();
+		int sum = ref.add(5,6);
+		int mult = ref.multiply(5,6);
+		System.out.println(sum);
+		System.out.println(mult);
+		System.out.println("just a addition in the project");
+		System.out.println("another addition to the git");
+		
+
+	}
+
+}
